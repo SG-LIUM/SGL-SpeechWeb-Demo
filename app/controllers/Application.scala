95b7954148bf141798db642df24271c22937f395
Vincent Jousse <vincent@jousse.org>
2013-06-06 10:11:41 +0200

    Tweak basic templating

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,12 +5,20 @@ import play.api.mvc._
 
 object Application extends Controller {
 
-  def index = Action {
-    Ok(views.html.index())
+  def about = Action {
+    Ok(views.html.about())
+  }
+
+  def demo = Action {
+    Ok(views.html.demo())
   }
 
   def docs = Action {
     Ok(views.html.docs())
+  }
+
+  def index = Action {
+    Ok(views.html.index())
   }
 
 }
