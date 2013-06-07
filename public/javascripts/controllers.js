c350b7cb2c18df3306ef7ea7d89c7f567fa6fe51
Vincent Jousse <vincent@jousse.org>
2013-06-07 11:41:27 +0200

    Sync the transcription with the media file

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -1,3 +1,46 @@
 function TranscriptionCtrl($scope, Restangular) {
-  $scope.transcriptions = Restangular.one('audiofiles.json', 6).getList('transcriptions')
+
+  var displayWordStart = 0;
+  var step = 50;
+  var nextTimeToDisplay = 0;
+
+  $scope.getNextWords = function(transcriptions) {
+    var displayWordEnd = displayWordStart + step;
+
+    // Try to avoid out of bounds exception
+    if (displayWordStart > transcriptions[0].length -1) {
+      return [];
+    }
+
+    if (displayWordEnd > transcriptions[0].length -1) {
+      displayWordEnd = transcriptions[0].length -1;
+    }
+
+    var words = transcriptions[0].content.slice(displayWordStart, displayWordEnd);
+    displayWordStart = displayWordEnd;
+    displayWordEnd = displayWordStart + step;
+    nextTimeToDisplay = transcriptions[0].content[displayWordStart].start;
+    console.log("Next word to display " + nextTimeToDisplay);
+    return words;
+  }
+
+  $scope.transcription = [];
+
+  Restangular.one('audiofiles.json', 6).getList('transcriptions').then(function(transcriptions) {
+    console.log(transcriptions);
+    $scope.fullTranscription = transcriptions;
+    $scope.transcription = $scope.getNextWords($scope.fullTranscription);
+  });
+
+
+  $("#mediafile").on("timeupdate", function (e) {
+
+    $scope.$apply( function() {
+      if(nextTimeToDisplay != 0 && e.target.currentTime > nextTimeToDisplay) {
+        $scope.transcription = $scope.getNextWords($scope.fullTranscription);
+      }
+    });
+
+  });
+
 }
