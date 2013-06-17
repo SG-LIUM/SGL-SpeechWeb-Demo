500acfb49c666784d3f2146428af18512d609ed4
Vincent Jousse <vincent@jousse.org>
2013-06-17 13:18:25 +0200

    Add autoinc to the AudioFile table

--- app/tables/AudioFileTable.scala
+++ app/tables/AudioFileTable.scala
@@ -3,9 +3,9 @@ package tables
 
 import scala.slick.driver.SQLiteDriver.simple._
 
-object AudioFiles extends Table[(Int, String)]("audio_files") {
-  def id = column[Int]("SUP_ID", O.PrimaryKey) // This is the primary key column
+object AudioFiles extends Table[(Option[Int], String)]("audio_files") {
+  def id = column[Int]("SUP_ID", O.PrimaryKey, O.AutoInc) // This is the primary key column
   def name = column[String]("SUP_NAME")
   // Every table needs a * projection with the same type as the table's type parameter
-  def * = id ~ name
+  def * = id.? ~ name
 }
