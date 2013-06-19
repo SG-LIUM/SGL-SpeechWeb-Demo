56996d2a5159323d6792ced4498f2566824e3538
Vincent Jousse <vincent@jousse.org>
2013-06-19 17:05:52 +0200

    Add optional filename (ctm) to Transcription

--- app/model/Transcription.scala
+++ app/model/Transcription.scala
@@ -1,10 +1,13 @@
 package fr.lium
 package model
 
+import java.io.File
+
 case class TranscriptionFinished(
   file: AudioFile,
   system: Option[String] = None,
-  transcription: List[Word])
+  transcription: List[Word],
+  filename: Option[File] = None)
 
 case class TranscriptionInProgress(
   file: AudioFile,
