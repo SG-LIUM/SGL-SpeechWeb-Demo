7e1deaebfcc4566d66ac3f8997e19a10f199571a
eSim <egithub@laposte.net>
2013-08-20 10:08:39 +0200

    Revert "Revert "Correction and comments""
    This reverts commit f443cf8c17fa5d234451f5a9323a00d45ccf72d4.

--- public/javascripts/angular/app.js
+++ public/javascripts/angular/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /* App Module */
-angular.module('liumsg', ['restangular', 'searchServices', 'transcriptionServices', 'ngUpload','fileParsing'])
+angular.module('liumsg', ['restangular', 'searchServices', 'transcriptionServices', 'ngUpload','fileServices'])
   .config(function(RestangularProvider) {
     RestangularProvider.setBaseUrl("http://localhost\\:9000");
   });
