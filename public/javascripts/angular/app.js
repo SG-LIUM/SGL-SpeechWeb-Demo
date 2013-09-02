8798b942bd77539be6b9a4d3370a7d5d95d1acb7
eSim <egithub@laposte.net>
2013-09-02 14:46:12 +0200

    Tooltip about word start

--- public/javascripts/angular/app.js
+++ public/javascripts/angular/app.js
@@ -4,4 +4,13 @@
 angular.module('liumsg', ['restangular', 'searchServices', 'transcriptionServices', 'ngUpload','fileServices','videoServices','positionServices','controllerServices'])
   .config(function(RestangularProvider) {
     RestangularProvider.setBaseUrl("http://localhost\\:9000");
-  });
+  }).directive('tooltip', function () {
+    return {
+        restrict:'A',
+        link: function(scope, element, attrs)
+        {
+            $(element)
+                .attr('title',scope.$eval(attrs.tooltip));
+        }
+    }
+});
